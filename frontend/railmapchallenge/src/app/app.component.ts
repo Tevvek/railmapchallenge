1e10777dc7af08a136cfdd6df4e0cf7f2177bfe4
Daniil Teterevkov <daniil.teterevkov@gmail.com>
2018-10-03 19:49:33 +0200

    el mapa se ve

--- frontend/railmapchallenge/src/app/app.component.ts
+++ frontend/railmapchallenge/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+  cityCenter = [41.390205, 2.154007];
+  ngOnInit() {
+    var map = L.map('map').setView(this.cityCenter, 20);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    // var marker = L.marker(this.cityCenter).addTo(map);
+    // map.on("contextmenu", function (event) {
+    //   console.log("user right-clicked on map coordinates: " + event.latlng.toString());
+    //   L.marker(event.latlng).addTo(map);
+    // });
+  }
+
 }
